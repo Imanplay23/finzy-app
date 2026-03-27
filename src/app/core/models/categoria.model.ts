35c31009091bbcfc493af29b304e712871b8e569
Imanplay23 <yandisanchez0321@gmail.com>
2026-03-27 19:50:53 -0400

    fix: icons names errors

--- src/app/core/models/categoria.model.ts
+++ src/app/core/models/categoria.model.ts
@@ -6,11 +6,11 @@ export interface Categoria {
 }
 
 export const CATEGORIAS_DEFAULT: Categoria[] = [
-  { id: 'alimentacion', nombre: 'Alimentación', icono: 'restaurant',    color: '#FF6384' },
-  { id: 'transporte',   nombre: 'Transporte',   icono: 'car',           color: '#36A2EB' },
-  { id: 'vivienda',     nombre: 'Vivienda',     icono: 'home',          color: '#CC65FE' },
-  { id: 'ocio',         nombre: 'Ocio',         icono: 'game-controller',color: '#FF9F40' },
-  { id: 'salud',        nombre: 'Salud',        icono: 'medkit',        color: '#4BC0C0' },
-  { id: 'educacion',    nombre: 'Educación',    icono: 'school',        color: '#FFCE56' },
-  { id: 'otros',        nombre: 'Otros',        icono: 'ellipsis-horizontal', color: '#9966FF' },
+  { id: 'alimentacion', nombre: 'Alimentación',  icono: 'restaurant-outline',          color: '#FF6384' },
+  { id: 'transporte',   nombre: 'Transporte',    icono: 'car-outline',                 color: '#36A2EB' },
+  { id: 'vivienda',     nombre: 'Vivienda',      icono: 'home-outline',                color: '#CC65FE' },
+  { id: 'ocio',         nombre: 'Ocio',          icono: 'game-controller-outline',     color: '#FF9F40' },
+  { id: 'salud',        nombre: 'Salud',         icono: 'medkit-outline',              color: '#4BC0C0' },
+  { id: 'educacion',    nombre: 'Educación',     icono: 'school-outline',              color: '#FFCE56' },
+  { id: 'otros',        nombre: 'Otros',         icono: 'ellipsis-horizontal-outline', color: '#9966FF' },
 ];
